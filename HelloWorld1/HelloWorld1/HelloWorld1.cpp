63cd7846c415b2ec1729d40bfc56756f72c3d638
fifoosid <fifoosid@gmail.com>
2016-10-06 13:53:06 +0300

    Add files via upload

---
+++ HelloWorld1/HelloWorld1/HelloWorld1.cpp
@@ -0,0 +1,18 @@
+// HelloWorld1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{	
+	int a, b, c;
+	cin >> a >> b;
+	c = a + b;
+	cout << c << endl;
+	//cout << "Hello World" << endl;
+}
+
