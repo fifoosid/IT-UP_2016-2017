b245f53d5a56d6bdd860a98a60e067bd2f5d13cb
fifoosid <fifoosid@gmail.com>
2016-10-10 10:19:22 +0300

    Add files via upload

---
+++ Dali_leji_v_zashtrihovanata_figura/Dali_leji_v_zashtrihovanata_figura/Dali_leji_v_zashtrihovanata_figura.cpp
@@ -0,0 +1,26 @@
+// Dali_leji_v_zashtrihovanata_figura.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+
+int main()
+{
+	int x, y, r1=2, r2=1;
+	cin >> x >> y;
+	if (((x - 0)*(x - 0) + (y - 0)*(y - 0) < r1*r1) || ((x - 3)*(x - 3) + (y - 3)*(y - 3) < r2*r2)) {
+		cout << "Tochkata leji v zashtrihovanata chast na figurata" << endl;
+	}
+	else {
+		cout << "Tochkata NE leji v zashtrihovanata chast na figurata" << endl;
+	}
+
+		
+
+    return 0;
+}
+
